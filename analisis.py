a87fc74d896ca840bb228f938f8e570d32932c52
Francisco1583 <A01737275@tec.mx>
2026-05-25 14:31:15 -0600

    variables ID

--- analisis.py
+++ analisis.py
@@ -1,11 +1,9 @@
-# %%
 import ply.lex as lex
 import ply.yacc as yacc
-from arbol import Literal, BinaryOp, Program, Assignment, Declaration, Declarations
+from arbol import Literal, BinaryOp, Program, Assignment, Declaration, Declarations, Statements, Variable, Visitor
+from llvmlite import ir
 
-# literals = ['+','-','*','/', '%', '(', ')']
 tokens = ['ID', 'INTLIT']
-
 t_ignore  = ' \t'
 literals = '+-*/%(){},;='
 
@@ -26,109 +24,61 @@ def t_error(t):
     print(f"Illegal character '{t.value[0]}'")
     t.lexer.skip(1)
 
-# %%
 def p_Program(p):
-    """
-    Program : ID ID '(' ')' '{' Declarations Statements '}'
-    """
+    """Program : ID ID '(' ')' '{' Declarations Statements '}'"""
     p[0] = Program(p[6], p[7])
 
 def p_Declarations(p):
-    """
-    Declarations : Declarations Declaration
-                 | Declaration
-    """
-    if len(p) == 2:
-        p[0] = Declarations(None, p[1])
-    else:
-        p[0] = Declarations(p[1], p[2])
-
+    """Declarations : Declarations Declaration
+                  | Declaration"""
+    p[0] = Declarations(p[1], p[2]) if len(p) == 3 else Declarations(None, p[1])
 
 def p_Declaration(p):
-    """
-    Declaration : ID ID ';'
-    """
+    """Declaration : ID ID ';'"""
     p[0] = Declaration(p[2], p[1])
 
 def p_Statements(p):
-    """
-    Statements : Statements Statement
-               | Statement
-    """
-    if len(p) == 2:
-        p[0] = p[1]
-    else:
-        raise
+    """Statements : Statements Statement
+                | Statement"""
+    # Antes aquí había un raise; ahora se conserva la lista enlazada.
+    p[0] = Statements(p[1], p[2]) if len(p) == 3 else Statements(None, p[1])
 
 def p_Statement(p):
-    """
-    Statement : Assignment
-    """
+    """Statement : Assignment"""
     p[0] = p[1]
 
 def p_Assignment(p):
-    """
-    Assignment : ID '=' Expression ';'
-    """
+    """Assignment : ID '=' Expression ';'"""
     p[0] = Assignment(p[1], p[3])
 
 def p_Expression(p):
-    """
-    Expression : Expression '+' Term
-               | Term
-    """
-    if len(p) == 2:
-        p[0] = p[1]
-    else:
-        p[0] = BinaryOp('+', p[1], p[3]) #('+', p[1], p[3])
-
+    """Expression : Expression '+' Term
+                  | Term"""
+    p[0] = BinaryOp('+', p[1], p[3]) if len(p) == 4 else p[1]
 
 def p_Term(p):
-    """
-    Term : Term '*' Factor
-         | Factor
-    """
-    if len(p) == 2:
-        p[0] = p[1]
-    else:
-        p[0] = BinaryOp('*', p[1], p[3]) # ('*', p[1], p[3])
+    """Term : Term '*' Factor
+            | Factor"""
+    p[0] = BinaryOp('*', p[1], p[3]) if len(p) == 4 else p[1]
 
 def p_Factor(p):
-    '''
-    Factor : INTLIT 
-            | '(' Expression ')'
-    '''
-    if len(p) == 2:
-        p[0] = Literal(p[1], 'INT')  #('LIT', 'INT', p[1])
-    else:
+    """Factor : INTLIT
+              | ID
+              | '(' Expression ')'"""
+    if len(p) == 4:
         p[0] = p[2]
-        
+    elif isinstance(p[1], int):
+        p[0] = Literal(p[1], 'INT')
+    else:
+        p[0] = Variable(p[1])
+
 def p_error(p):
     print("Syntax error in input!", p)
 
-
-# %%
-# from arbol import Calculator
-# data = '10 + 5 * 3'
-# lexer = lex.lex()
-# parser = yacc.yacc()
-# root = parser.parse(data)
-# calc = Calculator()
-# root.accept(calc)
-
-# %%
-# calc.stack
-# %%
-from arbol import Visitor, Variable
-from llvmlite import ir
-
 intType = ir.IntType(32)
 module = ir.Module(name="prog")
-
-# int main() {
 fnty = ir.FunctionType(intType, [])
 func = ir.Function(module, fnty, name='main')
-
 entry = func.append_basic_block('entry')
 builder = ir.IRBuilder(entry)
 
@@ -138,71 +88,60 @@ class IRGenerator(Visitor):
         self.symbol_table = {}
 
     def visit_declarations(self, node: Declarations) -> None:
-        node.decl.accept(self)
         if node.decls is not None:
             node.decls.accept(self)
+        node.decl.accept(self)
+
+    def visit_statements(self, node: Statements) -> None:
+        if node.stmts is not None:
+            node.stmts.accept(self)
+        node.stmt.accept(self)
 
     def visit_declaration(self, node: Declaration) -> None:
         self.symbol_table[node.variable] = builder.alloca(intType, name=node.variable)
 
     def visit_literal(self, node: Literal) -> None:
-        self.stack.append(
-            intType(node.value)
-        )
+        self.stack.append(ir.Constant(intType, node.value))
 
     def visit_program(self, node: Program) -> None:
         node.decls.accept(self)
         node.stmts.accept(self)
+        builder.ret(ir.Constant(intType, 0))
 
     def visit_assignment(self, node: Assignment) ->  None:
         node.expression.accept(self)
         tmp = self.stack.pop()
         if node.variable not in self.symbol_table:
-            raise
-        else:
-            builder.store(tmp, self.symbol_table[node.variable])
+            raise NameError(f"Variable no declarada: {node.variable}")
+        builder.store(tmp, self.symbol_table[node.variable])
 
     def visit_variable(self, node: Variable) -> None:
-        pass
+        if node.name not in self.symbol_table:
+            raise NameError(f"Variable no declarada: {node.name}")
+        self.stack.append(builder.load(self.symbol_table[node.name], name=node.name))
 
     def visit_binary_op(self, node: BinaryOp) -> None:
         node.lhs.accept(self)
         node.rhs.accept(self)
         rhs = self.stack.pop()
         lhs = self.stack.pop()
-
         if node.op == '+':
-            self.stack.append(
-                builder.add(lhs, rhs)
-            )
+            self.stack.append(builder.add(lhs, rhs))
         elif node.op == '*':
-            self.stack.append(
-                builder.mul(lhs, rhs)
-            )
+            self.stack.append(builder.mul(lhs, rhs))
 
-
-# %%
-data = """
-int main() 
-{
-    int f;
-    int i;
-
-    f = 10;
-}
-"""
-lexer = lex.lex()
-parser = yacc.yacc()
-root = parser.parse(data)
-
-print(root)
-irgen = IRGenerator()
-root.accept(irgen)
-
-# builder.ret(irgen.stack.pop())
-
-print(module)
-
-# %%
-irgen.stack
-# %%
+if __name__ == '__main__':
+    data = """
+    int main() {
+        int f;
+        int i;
+        f = 10;
+        i = f + 2;
+    }
+    """
+    lexer = lex.lex()
+    parser = yacc.yacc()
+    root = parser.parse(data)
+    irgen = IRGenerator()
+    root.accept(irgen)
+    print(module)
--- arbol.py
+++ arbol.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 from abc import ABC, abstractmethod
 from typing import Any
 
+
 class ASTNode(ABC):
     @abstractmethod
     def accept(self, visitor: Visitor) -> None:
@@ -24,13 +25,20 @@ class Declaration(ASTNode):
         visitor.visit_declaration(self)
 
 class Declarations(ASTNode):
-    def __init__(self, decls: Declarations, decl: Declaration) -> None:
+    def __init__(self, decls: Declarations | None, decl: Declaration) -> None:
         self.decls = decls
         self.decl = decl
 
     def accept(self, visitor: Visitor):
         visitor.visit_declarations(self)
 
+class Statements(ASTNode):
+    def __init__(self, stmts: Statements | None, stmt: ASTNode) -> None:
+        self.stmts = stmts
+        self.stmt = stmt
+
+    def accept(self, visitor: Visitor):
+        visitor.visit_statements(self)
 
 class Assignment(ASTNode):
     def __init__(self, variable: Any, expression: ASTNode) -> None:
@@ -52,7 +60,7 @@ class Literal(ASTNode):
         return f"[LIT, {self.value}]"
 
 class Variable(ASTNode):
-    def __init__(self, name: Any, type: str) -> None:
+    def __init__(self, name: Any, type: str = 'INT') -> None:
         self.name = name
         self.type = type
 
@@ -73,24 +81,53 @@ class BinaryOp(ASTNode):
 
 class Visitor(ABC):
     @abstractmethod
-    def visit_literal(self, node: Literal) -> None:
-        pass
+    def visit_program(self, node: Program) -> None: pass
     @abstractmethod
-    def visit_variable(self, node: Variable) -> None:
-        pass
+    def visit_declarations(self, node: Declarations) -> None: pass
     @abstractmethod
-    def visit_binary_op(self, node: BinaryOp) -> None:
-        pass
+    def visit_statements(self, node: Statements) -> None: pass
+    @abstractmethod
+    def visit_declaration(self, node: Declaration) -> None: pass
+    @abstractmethod
+    def visit_assignment(self, node: Assignment) -> None: pass
+    @abstractmethod
+    def visit_literal(self, node: Literal) -> None: pass
+    @abstractmethod
+    def visit_variable(self, node: Variable) -> None: pass
+    @abstractmethod
+    def visit_binary_op(self, node: BinaryOp) -> None: pass
 
 class Calculator(Visitor):
     def __init__(self):
         self.stack = []
+        self.values = {}
+
+    def visit_program(self, node: Program) -> None:
+        node.decls.accept(self)
+        node.stmts.accept(self)
+
+    def visit_declarations(self, node: Declarations) -> None:
+        if node.decls is not None:
+            node.decls.accept(self)
+        self.values.setdefault(node.decl.variable, 0)
+
+    def visit_statements(self, node: Statements) -> None:
+        if node.stmts is not None:
+            node.stmts.accept(self)
+        node.stmt.accept(self)
+
+    def visit_declaration(self, node: Declaration) -> None:
+        self.values.setdefault(node.variable, 0)
+
+    def visit_assignment(self, node: Assignment) -> None:
+        node.expression.accept(self)
+        self.values[node.variable] = self.stack.pop()
 
     def visit_literal(self, node: Literal) -> None:
         self.stack.append(node.value)
 
     def visit_variable(self, node: Variable) -> None:
-        pass
+        self.stack.append(self.values[node.name])
 
     def visit_binary_op(self, node: BinaryOp) -> None:
         node.lhs.accept(self)
@@ -99,11 +136,5 @@ class Calculator(Visitor):
         lhs = self.stack.pop()
         if node.op == '+':
             self.stack.append(lhs + rhs)
-        elif node.op == '-':
-            self.stack.append(lhs - rhs)
         elif node.op == '*':
             self.stack.append(lhs * rhs)
-        elif node.op == '/':
-            self.stack.append(lhs / rhs)
-        elif node.op == '%':
-            self.stack.append(lhs % rhs)
