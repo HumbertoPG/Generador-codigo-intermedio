9014e57ff755ae3d77b3912de7a02f624d1de93b
Alejandro Santana Moreno <A01733717@tec.mx>
2026-05-25 15:39:45 -0600

    refactor: structure AST around functions and blocks

--- analisis.py
+++ analisis.py
@@ -1,65 +1,85 @@
 import ply.lex as lex
 import ply.yacc as yacc
-from arbol import Literal, BinaryOp, Program, Assignment, Declaration, Declarations, Statements, Variable, Visitor
+from arbol import *
 from llvmlite import ir
 
-tokens = ['ID', 'INTLIT']
-t_ignore  = ' \t'
-literals = '+-*/%(){},;='
+keywords = {}
+tokens = ['ID', 'INTLIT'] + list(keywords.values())
+literals = '+-*/%(){};='
+t_ignore = ' \t\r'
 
-def t_ID(t):
-     r'[a-zA-Z_][a-zA-Z_0-9]*'
-     return t
 
 def t_INTLIT(t):
-    r'[0-9]+'
+    r'\d+'
     t.value = int(t.value)
     return t
+
+def t_ID(t):
+    r'[a-zA-Z_][a-zA-Z_0-9]*'
+    t.type = keywords.get(t.value, 'ID')
+    return t
+
+def t_COMMENT(t):
+    r'//.*'
+    pass
 
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
 
 def t_error(t):
-    print(f"Illegal character '{t.value[0]}'")
+    print(f"Error léxico: '{t.value[0]}' en línea {t.lexer.lineno}")
     t.lexer.skip(1)
 
+# --- Parser ---
+
 def p_Program(p):
-    """Program : ID ID '(' ')' '{' Declarations Statements '}'"""
-    p[0] = Program(p[6], p[7])
+    """Program : ID ID '(' ')' Block"""
+    p[0] = Program([FunctionDecl(p[1], p[2], [], p[5])])
+
+
+def p_Block(p):
+    """Block : '{' Declarations Statements '}' """
+    p[0] = Block(p[2], p[3])
 
 def p_Declarations(p):
     """Declarations : Declarations Declaration
-                  | Declaration"""
-    p[0] = Declarations(p[1], p[2]) if len(p) == 3 else Declarations(None, p[1])
+                    | empty"""
+    p[0] = p[1] + [p[2]] if len(p) == 3 else []
 
 def p_Declaration(p):
-    """Declaration : ID ID ';'"""
+    """Declaration : ID ID ';' """
     p[0] = Declaration(p[2], p[1])
+
 
 def p_Statements(p):
     """Statements : Statements Statement
-                | Statement"""
-    # Antes aquí había un raise; ahora se conserva la lista enlazada.
-    p[0] = Statements(p[1], p[2]) if len(p) == 3 else Statements(None, p[1])
+                  | empty"""
+    p[0] = p[1] + [p[2]] if len(p) == 3 else []
 
 def p_Statement(p):
     """Statement : Assignment"""
     p[0] = p[1]
 
 def p_Assignment(p):
-    """Assignment : ID '=' Expression ';'"""
+    """Assignment : ID '=' Expression ';' """
     p[0] = Assignment(p[1], p[3])
 
+
 def p_Expression(p):
-    """Expression : Expression '+' Term
-                  | Term"""
-    p[0] = BinaryOp('+', p[1], p[3]) if len(p) == 4 else p[1]
+    """Expression : Relation"""
+    p[0] = p[1]
+
+
+def p_Relation(p):
+    """Relation : Relation '+' Term
+                | Term"""
+    p[0] = BinaryOp(p[2], p[1], p[3]) if len(p) == 4 else p[1]
 
 def p_Term(p):
     """Term : Term '*' Factor
             | Factor"""
-    p[0] = BinaryOp('*', p[1], p[3]) if len(p) == 4 else p[1]
+    p[0] = BinaryOp(p[2], p[1], p[3]) if len(p) == 4 else p[1]
 
 def p_Factor(p):
     """Factor : INTLIT
@@ -69,79 +89,134 @@ def p_Factor(p):
         p[0] = p[2]
     elif isinstance(p[1], int):
         p[0] = Literal(p[1], 'INT')
-    else:
+    elif isinstance(p[1], float):
+        p[0] = Literal(p[1], 'FLOAT')
+    elif isinstance(p[1], str) and p.slice[1].type == 'STRING_LITERAL':
+        p[0] = Literal(p[1], 'STRING')
+    elif isinstance(p[1], str):
         p[0] = Variable(p[1])
+    else:
+        p[0] = p[1]
+
+
+def p_empty(p):
+    """empty :"""
+    pass
 
 def p_error(p):
-    print("Syntax error in input!", p)
+    if p:
+        print(f"Error de sintaxis en '{p.value}' (línea {p.lineno})")
+    else:
+        print("Error de sintaxis: Fin de archivo inesperado")
 
+# --- Generador LLVM IR ---
 intType = ir.IntType(32)
-module = ir.Module(name="prog")
-fnty = ir.FunctionType(intType, [])
-func = ir.Function(module, fnty, name='main')
-entry = func.append_basic_block('entry')
-builder = ir.IRBuilder(entry)
+floatType = ir.FloatType()
+voidptr_ty = ir.IntType(8).as_pointer()
 
 class IRGenerator(Visitor):
-    def __init__(self):
-        self.stack = []
+    def __init__(self, module):
+        self.module = module
+        self.builder = None
         self.symbol_table = {}
+        self.stack = []
+        self.current_function = None
 
-    def visit_declarations(self, node: Declarations) -> None:
-        if node.decls is not None:
-            node.decls.accept(self)
-        node.decl.accept(self)
+        self.printf = None
 
-    def visit_statements(self, node: Statements) -> None:
-        if node.stmts is not None:
-            node.stmts.accept(self)
-        node.stmt.accept(self)
 
-    def visit_declaration(self, node: Declaration) -> None:
-        self.symbol_table[node.variable] = builder.alloca(intType, name=node.variable)
+    def cast_types(self, lhs, rhs):
+        return lhs, rhs
 
-    def visit_literal(self, node: Literal) -> None:
-        self.stack.append(ir.Constant(intType, node.value))
+    def ir_type(self, source_type: str):
+        if source_type == 'float':
+            return floatType
+        return intType
 
-    def visit_program(self, node: Program) -> None:
-        node.decls.accept(self)
-        node.stmts.accept(self)
-        builder.ret(ir.Constant(intType, 0))
+    def visit_program(self, node: Program):
+        for func in node.functions:
+            func.accept(self)
 
-    def visit_assignment(self, node: Assignment) ->  None:
+    def visit_function_decl(self, node: FunctionDecl):
+        ret_ty = self.ir_type(node.ret_type)
+        param_types = [self.ir_type(p.type) for p in node.params]
+        fnty = ir.FunctionType(ret_ty, param_types)
+        func = ir.Function(self.module, fnty, name=node.name)
+        self.current_function = func
+        block = func.append_basic_block('entry')
+        self.builder = ir.IRBuilder(block)
+        self.symbol_table = {}
+        for i, param in enumerate(node.params):
+            arg = func.args[i]
+            arg.name = param.variable
+            ptr = self.builder.alloca(arg.type, name=param.variable)
+            self.builder.store(arg, ptr)
+            self.symbol_table[param.variable] = ptr
+        node.block.accept(self)
+        if node.name == 'main' and not self.builder.block.is_terminated:
+            self.builder.ret(ir.Constant(intType, 0))
+
+    def visit_block(self, node: Block):
+        for decl in node.decls:
+            decl.accept(self)
+        for stmt in node.stmts:
+            stmt.accept(self)
+
+    def visit_declaration(self, node: Declaration):
+        ty = self.ir_type(node.type)
+        ptr = self.builder.alloca(ty, name=node.variable)
+        self.symbol_table[node.variable] = ptr
+
+    def visit_assignment(self, node: Assignment):
         node.expression.accept(self)
-        tmp = self.stack.pop()
+        value = self.stack.pop()
         if node.variable not in self.symbol_table:
             raise NameError(f"Variable no declarada: {node.variable}")
-        builder.store(tmp, self.symbol_table[node.variable])
+        ptr = self.symbol_table[node.variable]
+        if isinstance(value.type, ir.IntType) and isinstance(ptr.type.pointee, ir.FloatType):
+            value = self.builder.sitofp(value, floatType)
+        elif isinstance(value.type, ir.FloatType) and isinstance(ptr.type.pointee, ir.IntType):
+            value = self.builder.fptosi(value, intType)
+        self.builder.store(value, ptr)
 
-    def visit_variable(self, node: Variable) -> None:
+    def visit_variable(self, node: Variable):
         if node.name not in self.symbol_table:
             raise NameError(f"Variable no declarada: {node.name}")
-        self.stack.append(builder.load(self.symbol_table[node.name], name=node.name))
+        ptr = self.symbol_table[node.name]
+        self.stack.append(self.builder.load(ptr, name=node.name))
 
-    def visit_binary_op(self, node: BinaryOp) -> None:
+    def visit_literal(self, node: Literal):
+        if node.type == 'INT':
+            self.stack.append(ir.Constant(intType, node.value))
+
+    def visit_binary_op(self, node: BinaryOp):
         node.lhs.accept(self)
         node.rhs.accept(self)
         rhs = self.stack.pop()
         lhs = self.stack.pop()
-        if node.op == '+':
-            self.stack.append(builder.add(lhs, rhs))
-        elif node.op == '*':
-            self.stack.append(builder.mul(lhs, rhs))
+        lhs, rhs = self.cast_types(lhs, rhs)
+        is_float = isinstance(lhs.type, ir.FloatType)
+        res = None
+        if node.op == '+': res = self.builder.add(lhs, rhs)
+        elif node.op == '*': res = self.builder.mul(lhs, rhs)
+        if res is None:
+            raise NotImplementedError(f"Operador no soportado: {node.op}")
+        self.stack.append(res)
+
 
 if __name__ == '__main__':
+    lexer = lex.lex()
+    parser = yacc.yacc(write_tables=False)
     data = """
     int main() {
-        int f;
-        int i;
-        f = 10;
-        i = f + 2;
+        int x;
+        int y;
+        x = 2;
+        y = x + 3;
     }
     """
-    lexer = lex.lex()
-    parser = yacc.yacc()
-    root = parser.parse(data)
-    irgen = IRGenerator()
-    root.accept(irgen)
+    ast = parser.parse(data, lexer=lexer)
+    module = ir.Module(name='prog')
+    irgen = IRGenerator(module)
+    ast.accept(irgen)
     print(module)
--- arbol.py
+++ arbol.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 from abc import ABC, abstractmethod
-from typing import Any
+from typing import Any, List
 
 
 class ASTNode(ABC):
@@ -9,132 +9,77 @@ class ASTNode(ABC):
         pass
 
 class Program(ASTNode):
-    def __init__(self, decls: Any, stmts: ASTNode) -> None:
-        self.decls = decls
-        self.stmts = stmts
-
+    def __init__(self, functions: List[FunctionDecl]) -> None:
+        self.functions = functions
     def accept(self, visitor: Visitor):
         visitor.visit_program(self)
 
+class FunctionDecl(ASTNode):
+    def __init__(self, ret_type: str, name: str, params: List[Declaration], block: Block) -> None:
+        self.ret_type = ret_type
+        self.name = name
+        self.params = params
+        self.block = block
+    def accept(self, visitor: Visitor):
+        visitor.visit_function_decl(self)
+
+class Block(ASTNode):
+    def __init__(self, decls: List[Declaration], stmts: List[ASTNode]) -> None:
+        self.decls = decls
+        self.stmts = stmts
+    def accept(self, visitor: Visitor):
+        visitor.visit_block(self)
+
 class Declaration(ASTNode):
-    def __init__(self, variable: Any, type: Any) -> None:
+    def __init__(self, variable: str, type: str) -> None:
         self.variable = variable
         self.type = type
-
     def accept(self, visitor: Visitor):
         visitor.visit_declaration(self)
 
-class Declarations(ASTNode):
-    def __init__(self, decls: Declarations | None, decl: Declaration) -> None:
-        self.decls = decls
-        self.decl = decl
-
-    def accept(self, visitor: Visitor):
-        visitor.visit_declarations(self)
-
-class Statements(ASTNode):
-    def __init__(self, stmts: Statements | None, stmt: ASTNode) -> None:
-        self.stmts = stmts
-        self.stmt = stmt
-
-    def accept(self, visitor: Visitor):
-        visitor.visit_statements(self)
-
 class Assignment(ASTNode):
-    def __init__(self, variable: Any, expression: ASTNode) -> None:
+    def __init__(self, variable: str, expression: ASTNode) -> None:
         self.variable = variable
         self.expression = expression
-
     def accept(self, visitor: Visitor):
         visitor.visit_assignment(self)
+
+
+class BinaryOp(ASTNode):
+    def __init__(self, op: str, lhs: ASTNode, rhs: ASTNode) -> None:
+        self.op = op
+        self.lhs = lhs
+        self.rhs = rhs
+    def accept(self, visitor: Visitor):
+        visitor.visit_binary_op(self)
 
 class Literal(ASTNode):
     def __init__(self, value: Any, type: str) -> None:
         self.value = value
         self.type = type
-
     def accept(self, visitor: Visitor):
         visitor.visit_literal(self)
 
-    def __str__(self):
-        return f"[LIT, {self.value}]"
-
 class Variable(ASTNode):
-    def __init__(self, name: Any, type: str = 'INT') -> None:
+    def __init__(self, name: str) -> None:
         self.name = name
-        self.type = type
-
     def accept(self, visitor: Visitor):
         visitor.visit_variable(self)
 
-class BinaryOp(ASTNode):
-    def __init__(self, op: str, lhs: ASTNode, rhs: ASTNode) -> None:
-        self.lhs = lhs
-        self.rhs = rhs
-        self.op = op
-
-    def accept(self, visitor: Visitor):
-        visitor.visit_binary_op(self)
-
-    def __str__(self):
-        return f"[{self.op}, {self.lhs}, {self.rhs}]"
-
 class Visitor(ABC):
     @abstractmethod
-    def visit_program(self, node: Program) -> None: pass
+    def visit_program(self, node): pass
     @abstractmethod
-    def visit_declarations(self, node: Declarations) -> None: pass
+    def visit_function_decl(self, node): pass
     @abstractmethod
-    def visit_statements(self, node: Statements) -> None: pass
+    def visit_block(self, node): pass
     @abstractmethod
-    def visit_declaration(self, node: Declaration) -> None: pass
+    def visit_declaration(self, node): pass
     @abstractmethod
-    def visit_assignment(self, node: Assignment) -> None: pass
+    def visit_assignment(self, node): pass
     @abstractmethod
-    def visit_literal(self, node: Literal) -> None: pass
+    def visit_binary_op(self, node): pass
     @abstractmethod
-    def visit_variable(self, node: Variable) -> None: pass
+    def visit_literal(self, node): pass
     @abstractmethod
-    def visit_binary_op(self, node: BinaryOp) -> None: pass
-
-class Calculator(Visitor):
-    def __init__(self):
-        self.stack = []
-        self.values = {}
-
-    def visit_program(self, node: Program) -> None:
-        node.decls.accept(self)
-        node.stmts.accept(self)
-
-    def visit_declarations(self, node: Declarations) -> None:
-        if node.decls is not None:
-            node.decls.accept(self)
-        self.values.setdefault(node.decl.variable, 0)
-
-    def visit_statements(self, node: Statements) -> None:
-        if node.stmts is not None:
-            node.stmts.accept(self)
-        node.stmt.accept(self)
-
-    def visit_declaration(self, node: Declaration) -> None:
-        self.values.setdefault(node.variable, 0)
-
-    def visit_assignment(self, node: Assignment) -> None:
-        node.expression.accept(self)
-        self.values[node.variable] = self.stack.pop()
-
-    def visit_literal(self, node: Literal) -> None:
-        self.stack.append(node.value)
-
-    def visit_variable(self, node: Variable) -> None:
-        self.stack.append(self.values[node.name])
-
-    def visit_binary_op(self, node: BinaryOp) -> None:
-        node.lhs.accept(self)
-        node.rhs.accept(self)
-        rhs = self.stack.pop()
-        lhs = self.stack.pop()
-        if node.op == '+':
-            self.stack.append(lhs + rhs)
-        elif node.op == '*':
-            self.stack.append(lhs * rhs)
+    def visit_variable(self, node): pass
